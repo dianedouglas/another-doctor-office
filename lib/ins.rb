2fcfcf8278935f82befdff030574e5fd8329c653
Lee Kebler & Diane Douglas <student@epicodus.com>
2014-08-06 14:53:02 -0700

    add insurance class.

---
+++ lib/ins.rb
@@ -0,0 +1,7 @@
+class Ins
+attr_reader:name, :id
+  def initialize(name, id = nil)
+    @name = name
+    @id = id
+  end
+end
---
+++ spec/ins_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require 'doctor'
+require 'area'
+require 'pry'
+require 'pg'
+require 'ins'
+
+test_ins = Ins.new('Health Cross')
+test_doctor = Doctor.new("Dr. Who", 1)
+DB = PG.connect({:dbname => 'doctors_office'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec('DELETE FROM ins *;')
+  end
+end
+
+describe Ins do
+  it "intializes with a company name" do
+    expect(test_ins).to be_an_instance_of Ins
+  end
+
+  it "should return the name of the company" do
+    expect(test_ins.name).to eq 'Health Cross'
+  end
+
+end
